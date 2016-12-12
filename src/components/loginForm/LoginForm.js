32c5ae4cd5ba4dd20708021d075aef7e56d6c4bf
thehink <benja280@gmail.com>
2016-12-12 13:07:25 +0100

    Added login form

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -2,12 +2,35 @@ import './LoginForm.css';
 
 import React, { Component, PropTypes } from 'react';
 import {Link} from 'react-router';
+import FontAwesome from 'react-fontawesome';
+import { Form, Input, FormGroup, Col, Label, Button, ButtonGroup } from 'reactstrap';
+
+import Container from '../page';
 
 
 class LoginForm extends Component {
   render() {
     return (
-      <h2>LoginForm</h2>
+      <Container>
+        <h2>Login</h2>
+        <Form>
+          <FormGroup row>
+            <Label for="email" sm={2}>Email</Label>
+            <Col sm={10}>
+              <Input type="email" name="email" id="username" placeholder="Usernamer" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="password" sm={2}>Password</Label>
+            <Col sm={10}>
+              <Input type="password" name="password" id="password" placeholder="Password" />
+            </Col>
+          </FormGroup>
+          <ButtonGroup>
+            <Button type="submit" color="primary">Login</Button>
+          </ButtonGroup>
+        </Form>
+     </Container>
     );
   }
 }
