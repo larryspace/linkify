8700192606849a875f789efe096cb1a81b33ee92
Thehink <benja280@gmail.com>
2016-12-21 11:39:17 +0100

    Added input icon support

--- src/components/Forms/renderField.js
+++ src/components/Forms/renderField.js
@@ -1,13 +1,19 @@
 import React, { Component, PropTypes } from 'react';
 import { Form, Input, FormGroup, Col, Label,
-  Button, ButtonGroup, Alert, FormFeedback} from 'reactstrap';
+  Button, ButtonGroup, Alert, FormFeedback, InputGroupAddon, InputGroup} from 'reactstrap';
 
-export default ({ input, label, name, type, meta: { touched, error, warning } }) => {
+import FontAwesome from 'react-fontawesome';
+
+export default ({ input, label, name, type, icon, meta: { touched, error, warning } }) => {
   return (
   <FormGroup row color={touched && (error && 'danger' || warning && 'danger') || ''}>
     <Label for={name} sm={2}>{label}</Label>
     <Col sm={10}>
-      <Input {...input} type={type} placeholder={label} />
+      <InputGroup>
+        {icon && <InputGroupAddon><FontAwesome name={icon} /></InputGroupAddon>}
+        <Input {...input} type={type} placeholder={label} />
+      </InputGroup>
+
       {touched && ((error && <FormFeedback>{error}</FormFeedback>) || (warning && <FormFeedback>{warning}</FormFeedback>))}
     </Col>
   </FormGroup>)
