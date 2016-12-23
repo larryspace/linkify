dada5bd23f5ce76281b057b5829a8ebbbb086f8d
Thehink <benja280@gmail.com>
2016-12-23 01:22:58 +0100

    Added a profile card component

---
+++ src/components/ProfileCard/ProfileCard.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+import './ProfileCard.scss';
+
+export default class ProfileCard extends Component {
+  render() {
+    return (
+      <div className="profile-card">
+        <div className="profile-card-pic">
+        </div>
+        <span className="profile-card-name">Thehink <span className="tag tag-default">321</span></span>
+        <ul className="profile-card-info">
+          <li>
+            <a href="">
+              <span className="profile-card-info-title">Links</span>
+              <span className="profile-card-info-value">22</span>
+            </a>
+          </li>
+          <li>
+            <a href="">
+              <span className="profile-card-info-title">Comments</span>
+              <span className="profile-card-info-value">22</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
