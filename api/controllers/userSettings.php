ac046f399dfb3e610e284b784dd7e26cc638ae65
Thehink <benja280@gmail.com>
2016-12-31 13:39:53 +0100

    Avatar uploads should work

--- api/controllers/userSettings.php
+++ api/controllers/userSettings.php
@@ -18,8 +18,22 @@ class UserSettings
         $error = $_FILES['avatar']['error'];
         $size = $_FILES['avatar']['size'];
 
+        if($_FILES['avatar']['error'] > 0){
+            switch ($_FILES['avatar']['error']) {
+                case UPLOAD_ERR_OK:
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    throw new \ApiException('FormError', 400, ['avatar' => 'No file sent']);
+                case UPLOAD_ERR_INI_SIZE:
+                case UPLOAD_ERR_FORM_SIZE:
+                    throw new \ApiException('FormError', 400, ['avatar' => 'Exceeded filesize limit']);
+                default:
+                    throw new \ApiException('FormError', 400, ['avatar' => 'Unknown error']);
+            }
+        }
+
         if($size > 1024 * 1024 * 5) {
-            throw new \ApiException('FormError', 400, ['avatar' => 'Max filesize 5MB']);
+            //throw new \ApiException('FormError', 400, ['avatar' => 'Max filesize 5MB']);
         }
 
         $finfo = new \finfo(FILEINFO_MIME_TYPE);
@@ -36,12 +50,19 @@ class UserSettings
         }
 
         $ext = $fileTypes[$mime];
-        $newPath = sprintf('uploads/avatars/%s.%s',
+        $newPath = sprintf('uploads/avatars/%s.%s.%s',
                             sha1_file($_FILES['avatar']['tmp_name']),
+                            uniqid(),
                             $ext
                         );
 
         move_uploaded_file($_FILES['avatar']['tmp_name'], $newPath);
+
+        if($user->avatar){
+            unlink($user->avatar);
+        }
+
+        $user->_save(['avatar' => $newPath]);
 
         return ['avatar' => $newPath];
     }
