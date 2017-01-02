103068dfb77d8d8599145c0e4dcdb0cc1345e2a2
Thehink <benja280@gmail.com>
2017-01-02 15:13:32 +0100

    Require certain env variables

--- api/lib/autoload.php
+++ api/lib/autoload.php
@@ -3,6 +3,15 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
+$dotenv->required([
+    'DB_HOST',
+    'DB_NAME',
+    'DB_USER',
+    'DB_PASSWORD',
+    'TOKEN_ENCRYPTION_ALG',
+    'TOKEN_ENCRYPTION_KEY',
+    'TOKEN_ENCRYPTION_IV'
+]);
 
 require_once __DIR__ . '/apiException.php';
 require_once __DIR__ . '/helpers.php';
