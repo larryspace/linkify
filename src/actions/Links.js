37322b22bb3781a808e1c3a824b1aac3cd5d0a5c
Thehink <benja280@gmail.com>
2017-01-03 13:50:18 +0100

    Added action to post new link

--- src/actions/Links.js
+++ src/actions/Links.js
@@ -1,4 +1,7 @@
-import {GET_LINKS_REQUEST, GET_LINKS_FAILURE, GET_LINKS_SUCCESS} from '../constants/ActionTypes';
+import {
+  GET_LINKS_REQUEST, GET_LINKS_FAILURE, GET_LINKS_SUCCESS,
+  POST_NEW_LINK_REQUEST, POST_NEW_LINK_FAILURE, POST_NEW_LINK_SUCCESS
+} from '../constants/ActionTypes';
 import { CALL_API } from '../middleware/api';
 
 const getLinksRequest = ({ directory }) => ({
@@ -9,9 +12,22 @@ const getLinksRequest = ({ directory }) => ({
   }
 });
 
-export const getLinks = (values) => (dispatch, getState) =>  {
+const postNewLinkRequest = ({ title, link, directory }) => ({
+  [CALL_API]: {
+    types: [ POST_NEW_LINK_REQUEST, POST_NEW_LINK_SUCCESS, POST_NEW_LINK_FAILURE ],
+    endpoint: `d/${directory}/new`,
+    method: 'POST',
+    body: {
+      title,
+      link
+    }
+  }
+});
 
-  //const links = getState
-
+export const loadLinks = (values) => (dispatch, getState) =>  {
   return dispatch(getLinksRequest(values));
+};
+
+export const postNewLink = (values) => (dispatch, getState) =>  {
+  return dispatch(postNewLinkRequest(values));
 };
