cecad1d8cbc094b072aee5dbe166b8e380ffbf8c
Thehink <benja280@gmail.com>
2017-01-03 13:36:00 +0100

    Env bug seems to be fixed now

--- api/lib/autoload.php
+++ api/lib/autoload.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
-$dotenv->load();
+$dotenv->overload();
 $dotenv->required([
     'DB_HOST',
     'DB_NAME',
