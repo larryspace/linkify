dcdba78be739f5d730b2c6b513f80fdc8cc433f3
Thehink <benja280@gmail.com>
2017-01-03 13:52:57 +0100

    Handles posting new links & loading links

--- src/containers/Sub/SubContainer.js
+++ src/containers/Sub/SubContainer.js
@@ -5,22 +5,64 @@ import { connect } from 'react-redux';
 import Container from '../../components/Container';
 import SubHeader from '../../components/SubHeader';
 import SubList from '../../components/SubList';
+import ModalForm from '../../components/ModalForm';
+import NewLinkForm from '../../components/Forms/NewLinkForm';
 
-import { setPageInfo } from '../../actions';
-
+import { setPageInfo, submitForm, postNewLink, loadLinks } from '../../actions';
 
 class SubContainer extends Component {
 
+  state = {
+    formOpen: false,
+  }
+
   componentDidMount() {
-    this.props.setPageInfo({ title: '' });
+    const directory = this.props.params.directory;
+    this.props.setPageInfo({ title: '', directory: directory });
+    this.props.loadLinks({ directory });
+  }
+
+  componentWillReceiveProps(nextProps){
+    const newDirectory = nextProps.params.directory;
+    if(this.props.params.directory != newDirectory){
+      this.props.setPageInfo({ title: '', directory: newDirectory });
+      this.props.loadLinks({ directory: newDirectory });
+    }
+  }
+
+  onSubmitNewLink(values){
+    const directory = this.props.params.directory;
+    const data = {
+      ...values,
+      directory
+    };
+
+    return this.props.postNewLink(data)
+    .then(action => {
+      if(!action.error){
+        this.toggle();
+      }
+    });
+  }
+
+  toggleModal(){
+    this.setState({ formOpen: !this.state.formOpen});
   }
 
   render() {
-
     return (
       <div>
+      <ModalForm
+        isOpen={ this.state.formOpen }
+        toggle={ this.toggleModal.bind(this) }
+        disabled={ this.props.submitting }
+        form={ NewLinkForm }
+        onSubmit={ this.onSubmitNewLink.bind(this) }
+        onSubmitClick={() => this.props.submitForm('newLinkForm')}
+       />
       <SubHeader
         title={ "Title" }
+        onNewLinkClick={this.toggleModal.bind(this)}
       />
       <Container>
         <SubList />
@@ -32,13 +74,17 @@ class SubContainer extends Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-
+    directory: state.Page.directory,
+    submitting: state.form.newLinkForm && state.form.newLinkForm.submitting
   }
 }
 
 
 export default connect(mapStateToProps,
   {
-    setPageInfo
+    setPageInfo,
+    submitForm,
+    postNewLink,
+    loadLinks
   }
 )(SubContainer);
