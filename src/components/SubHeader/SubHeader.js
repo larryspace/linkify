d4e61dd8f4b75944b62e43379989841136d9f645
Thehink <benja280@gmail.com>
2017-01-03 13:48:45 +0100

    Added New Link & subscribe buttons to directory header

--- src/components/SubHeader/SubHeader.js
+++ src/components/SubHeader/SubHeader.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button, ButtonGroup } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
 
 import './SubHeader.scss';
 
@@ -7,6 +9,14 @@ export default class SubHeader extends Component {
     return (
       <div className="app-sub-header">
         <h3 className="app-sub-header-title">Title</h3>
+        <ButtonGroup className="pull-right">
+         <Button onClick={ this.props.onNewLinkClick } color="primary">
+          <FontAwesome name="plus" style={{ color: 'rgba(0,0,0,.5)' }} /> New Link
+         </Button>
+         <Button onClick={ this.props.onNewLinkClick } color="secondary">
+          <FontAwesome name="check" style={{ color: 'rgba(0,0,0,.5)' }} /> Subscribe
+         </Button>
+       </ButtonGroup>
       </div>
     );
   }
