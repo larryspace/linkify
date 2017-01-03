e49d05d3f9d3c269d884fc2b833d4e8f15521782
Thehink <benja280@gmail.com>
2017-01-03 13:46:18 +0100

    Added a ModalForm component

---
+++ src/components/ModalForm/ModalForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Button, ButtonGroup, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import FontAwesome from 'react-fontawesome';
+
+export default class ModalForm extends Component {
+  render() {
+    return (
+      <Modal isOpen={this.props.isOpen} toggle={this.props.toggle}>
+          <ModalHeader toggle={this.toggle}>New Link</ModalHeader>
+          <ModalBody>
+            <this.props.form onSubmit={this.props.onSubmit} />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" disabled={ this.props.disabled } onClick={this.props.onSubmitClick}>Submit</Button>{' '}
+            <Button color="secondary" onClick={ this.props.toggle }>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+    );
+  }
+}
