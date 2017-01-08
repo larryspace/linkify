8b6885f870600d2d2af2518114e9b3d8d16e2ae2
Thehink <benja280@gmail.com>
2017-01-09 00:57:35 +0100

    Added schemas for api

---
+++ src/middleware/schemas.js
@@ -0,0 +1,30 @@
+import { normalize, schema } from 'normalizr';
+
+const userSchema = new schema.Entity('users', {}, {
+  idAttribute: user => user.username.toLowerCase()
+});
+
+const linkSchema = new schema.Entity('links', {
+  user: userSchema
+});
+
+const directorySchema = new schema.Entity('directories', {}, {
+  idAttribute: directory => directory.name.toLowerCase()
+});
+
+const commentSchema = new schema.Entity('comments', {
+  user: userSchema
+});
+
+const Schemas = {
+  USER: userSchema,
+  USER_ARRAY: [userSchema],
+  LINK: linkSchema,
+  LINK_ARRAY: [linkSchema],
+  DIRECTORY: directorySchema,
+  DIRECTORY_ARRAY: [directorySchema],
+  COMMENT: commentSchema,
+  COMMENT_ARRAY: [commentSchema]
+};
+
+export default Schemas;
