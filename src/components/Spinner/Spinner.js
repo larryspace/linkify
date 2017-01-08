96c08b6067de00c9bbed4197eaf07cc2e4c9a23c
Thehink <benja280@gmail.com>
2017-01-09 00:07:39 +0100

    Added a bit of style to loading spinner

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,12 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-
 import FontAwesome from 'react-fontawesome';
 
+import './Spinner.scss';
+
 export default ({ component: Component, ...rest }) => (
+  <div className="spinner-wrapper">
   <FontAwesome
         name='circle-o-notch'
         size='2x'
         spin
         style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
       />
+    </div>
 )
