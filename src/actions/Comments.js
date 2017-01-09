72d02d311348e6d5225c61bc42782cf3ef0e373e
Thehink <benja280@gmail.com>
2017-01-09 18:14:19 +0100

    Changed fetch comments url

--- src/actions/Comments.js
+++ src/actions/Comments.js
@@ -6,7 +6,7 @@ import { CALL_API, Schemas } from '../middleware/api';
 const loadCommentsRequest = ({ link }) => ({
   [CALL_API]: {
     types: [ LOAD_COMMENTS_REQUEST, LOAD_COMMENTS_SUCCESS, LOAD_COMMENTS_FAILURE ],
-    endpoint: `d/link/${link}/comments`,
+    endpoint: `link/${link}/comments`,
     method: 'GET',
     schema: Schemas.COMMENT_ARRAY
   }
