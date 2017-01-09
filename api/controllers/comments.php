9ec543c36a8a4e0f8b4525f859cdf2a1a508315e
Thehink <benja280@gmail.com>
2017-01-09 18:13:22 +0100

    Added endpoint to get comments for a link

---
+++ api/controllers/comments.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\controllers;
+
+/**
+ *
+ */
+class Comments
+{
+
+    static function getLinkComments($params){
+        $id = (int)$params['link'];
+
+        return [];
+    }
+}
--- api/index.php
+++ api/index.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/controllers/user.php';
 require_once __DIR__ . '/controllers/userSettings.php';
 require_once __DIR__ . '/controllers/directory.php';
 require_once __DIR__ . '/controllers/links.php';
+require_once __DIR__ . '/controllers/comments.php';
 
 $router = new miniRoute();
 
@@ -18,21 +19,6 @@ class TestClass
     }
 }
 
-
-$router->GET("/test/:first/:second", 'TestClass::test');
-
-$router->GET("/post/:id/comments", 'CommentStore::getComments');
-$router->POST("/post/:id/comment", 'CommentStore::newComment', 'Authentication::requireAuth');
-$router->POST("/post/:id/comment/:commentId/reply", 'CommentStore::newCommentReply', 'Authentication::requireAuth');
-$router->POST("/post/:id/comment/:commentId/vote", 'CommentStore::voteComment', 'Authentication::requireAuth');
-$router->DELETE("/post/:id/comment/:commentId", 'CommentStore::deleteComment', 'Authentication::requireAuth');
-
-$router->POST("/post", 'PostStore::newPost', 'Authentication::requireAuth');
-$router->POST("/post/:id/vote", 'PostStore::votePost', 'Authentication::requireAuth');
-$router->POST("/post/:id", 'PostStore::updatePost', 'Authentication::requireAuth');
-$router->DELETE("/post/:id", 'PostStore::deletePost', 'Authentication::requireAuth');
-$router->GET("/posts(/:page)", 'PostStore::getPosts');
-
 $router->GET("/directories", 'app\controllers\Directory::getDefaultDirectories');
 $router->GET("/d/:directory", 'app\controllers\Directory::getDirectory');
 
@@ -40,6 +26,8 @@ $router->GET("/d/link/:id", 'app\controllers\Links::getLink');
 $router->GET("/d/:directory/:page/:sort", 'app\controllers\Links::getLinks');
 $router->POST("/d/:directory/new", 'app\controllers\Links::newLink', '\Authentication::requireAuth');
 $router->POST("/d/link/:id/:vote", 'app\controllers\Links::voteLink', '\Authentication::requireAuth');
+
+$router->GET("/link/:link/comments", 'app\controllers\Comments::getLinkComments');
 
 $router->POST("/account/info", 'app\controllers\UserSettings::updateInfo', '\Authentication::requireAuth');
 $router->POST("/account/avatar", 'app\controllers\UserSettings::updateAvatar', '\Authentication::requireAuth');
