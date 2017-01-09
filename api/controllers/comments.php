8f582bce75ee54ae33ef396eb013ba51d6fc5986
Thehink <benja280@gmail.com>
2017-01-09 23:56:28 +0100

    Generate some fakedata to experiment with

--- api/controllers/comments.php
+++ api/controllers/comments.php
@@ -7,9 +7,22 @@ namespace app\controllers;
 class Comments
 {
 
+    static $num = 1;
+
+    static function genFakeComments($parent = 0){
+        return [
+            'id' => self::$num++,
+            'parent' => $parent,
+            'author' => 'Thehink',
+            'content' => 'My Comment',
+            'created_at' => '',
+            'updated_at' => ''
+        ];
+    }
+
     static function getLinkComments($params){
         $id = (int)$params['link'];
 
-        return [];
+        return [self::genFakeComments(),self::genFakeComments(1),self::genFakeComments(1),self::genFakeComments(),self::genFakeComments(2),self::genFakeComments(3)];
     }
 }
