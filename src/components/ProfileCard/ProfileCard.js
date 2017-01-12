516d08060a2c8fdd7ab6f489a851e3920980de24
Thehink <benja280@gmail.com>
2017-01-13 00:22:33 +0100

    Updated ProfileCard component

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import { Badge } from 'reactstrap';
 
 import './ProfileCard.scss';
 
 export default class ProfileCard extends Component {
+
+  static propTypes = {
+    username: PropTypes.string,
+    avatar: PropTypes.string,
+  }
+
   render() {
+
+    const {
+      username,
+      avatar
+    } = this.props;
+
     return (
       <div className="profile-card">
-        <div className="profile-card-pic">
-        </div>
-        <span className="profile-card-name">{ this.props.username } <span className="tag tag-default">321</span></span>
+        <img className="profile-card-pic" src={ avatar ? '/' + avatar : '' }>
+        </img>
+        <span className="profile-card-name">{ username } <Badge>0</Badge></span>
         <ul className="profile-card-info">
           <li>
-            <a href="">
+            <Link to={'profile/links'}>
               <span className="profile-card-info-title">Links</span>
               <span className="profile-card-info-value">22</span>
-            </a>
+            </Link>
           </li>
           <li>
-            <a href="">
+            <Link to={'profile/comments'}>
               <span className="profile-card-info-title">Comments</span>
               <span className="profile-card-info-value">22</span>
-            </a>
+            </Link>
           </li>
         </ul>
       </div>
