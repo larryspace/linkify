280785ad095ab948ef40faf3b049d407c25399f8
Thehink <benja280@gmail.com>
2017-01-12 12:55:48 +0100

    Cleaned ModalForm

--- src/components/ModalForm/ModalForm.js
+++ src/components/ModalForm/ModalForm.js
@@ -1,18 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Button, ButtonGroup, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import FontAwesome from 'react-fontawesome';
 
 export default class ModalForm extends Component {
+
+  static propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    onSubmit: PropTypes.func.isRequired,
+    onSubmitClick: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+    form: PropTypes.func.isRequired,
+  }
+
   render() {
+
+    const {
+      isOpen,
+      toggle,
+      onSubmit,
+      onSubmitClick,
+      disabled,
+      title
+    } = this.props;
+
     return (
-      <Modal isOpen={this.props.isOpen} toggle={this.props.toggle}>
-          <ModalHeader toggle={this.toggle}>New Link</ModalHeader>
+      <Modal isOpen={ isOpen } toggle={ toggle }>
+          <ModalHeader>{ title }</ModalHeader>
           <ModalBody>
-            <this.props.form hideSubmit={ true } onSubmit={this.props.onSubmit} />
+            <this.props.form hideSubmit={ true } onSubmit={ onSubmit } />
           </ModalBody>
           <ModalFooter>
-            <Button color="primary" disabled={ this.props.disabled } onClick={this.props.onSubmitClick}>Submit</Button>{' '}
-            <Button color="secondary" onClick={ this.props.toggle }>Cancel</Button>
+            <Button color="primary" disabled={ disabled } onClick={ onSubmitClick }>Submit</Button>{' '}
+            <Button color="secondary" onClick={ toggle }>Cancel</Button>
           </ModalFooter>
         </Modal>
     );
