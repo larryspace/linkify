49d4b6bcaba0118aa8d7f3b5d9b6c4d3bac958b2
thehink <benja280@gmail.com>
2017-01-17 09:32:06 +0100

    Added comments

--- api/controllers/directory.php
+++ api/controllers/directory.php
@@ -6,18 +6,34 @@ namespace app\controllers;
  */
 class Directory
 {
+    /**
+     * Get a list of default directories
+     * @param  Array $params Array of params
+     * @return [app\models\Directory]         Array of Directories
+     */
     public static function getDefault($params)
     {
         $directories = \app\stores\Directory::getDefault();
         return $directories;
     }
 
+    /**
+     * Get a list of subscribed directories
+     * @param  Array $params Array of params
+     * @param  app\models\User $user   Authenticated user
+     * @return [app\models\Directory]         Array of Directories
+     */
     public static function getSubscribed($params, $user)
     {
         $directories = \app\stores\Directory::getSubscribed($user->id);
         return $directories;
     }
 
+    /**
+     * Get a single directory by name
+     * @param  Array $params Array of params
+     * @return app\models\Directory         Directory
+     */
     public static function getDirectory($params)
     {
         $directory = \app\stores\Directory::getDirectory($params['directory']);
@@ -29,6 +45,12 @@ class Directory
         return $directory;
     }
 
+    /**
+     * Subscribe to a directory
+     * @param  Array $params Array of params
+     * @param  app\models\User $user   Authenticated user
+     * @return app\models\Directory    Directory
+     */
     public static function subscribe($params, $user)
     {
         $directory = $params['directory'];
@@ -48,6 +70,12 @@ class Directory
         return ['name' => $directory->name];
     }
 
+    /**
+     * Unsubscribe to a directory
+     * @param  Array $params Array of params
+     * @param  app\models\User $user   Authenticated user
+     * @return app\models\Directory    Directory
+     */
     public static function unsubscribe($params, $user)
     {
         $directory = $params['directory'];
